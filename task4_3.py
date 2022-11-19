61df99cd04907db9a86f39ed7895222df135c305
Reinar Bjornsson <30556076+DeBleiden@users.noreply.github.com>
2022-11-19 09:11:28 +0200

    Fix

--- task4_3.py
+++ task4_3.py
@@ -1,12 +1,12 @@
+config = "switchport trunk allowed vlan 1,3,10,20,30,100"
 result = []
 
 
-def listGeneration():
-    config = "switchport trunk allowed vlan 1,3,10,20,30,100"
-    tempList = (config.replace('switchport trunk allowed vlan', ' ')).strip().split(',')
+def listGeneration(string):
+    tempList = (string.replace('switchport trunk allowed vlan', ' ')).strip().split(',')
     for i in tempList:
         result.append(i)
     print(result)
 
 
-listGeneration()
+listGeneration(config)
--- task4_8.py
+++ task4_8.py
@@ -2,10 +2,10 @@ ip = "192.168.3.1"
 ipTable = '{:<10}' * 4, '{:>08b}  ' * 4
 
 
-def ipOutput():
-    tempList = ip.split('.')
-    for i in ipTable:
+def ipOutput(string, tuples):
+    tempList = string.split('.')
+    for i in tuples:
         print(i.format(int(tempList[0]), int(tempList[1]), int(tempList[2]), int(tempList[3])))
 
 
-ipOutput()
+ipOutput(ip, ipTable)
